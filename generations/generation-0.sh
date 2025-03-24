ced321fb37ff5760bc1e477990b9a11affeb59e8
Manuel Palumbo <manuel.palumbo@email.com>
2025-03-24 14:39:36 +0100

    Inital Commit

---
+++ generations/generation-0.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+echo "This script is performing the following actions:"
+echo "- Install SDDM"
+echo "- Install Hyprland"
+echo "- Install Dekstop-Portals"
+echo "- Install Keyring"
+echo "- Install Audio & Screen utils"
+echo "- Install Stow"
+echo "- Install Hyprpanel"
+echo "- Install rofi"
+echo "- Install Kitty"
+
+# Hyprland Installation
+sudo pacman -S sddm \
+	       hyprland \
+	       noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ttf-jetbrains-mono-nerd \
+	       nwg-look papirus-icon-theme \
+	       xdg-desktop-portal xdg-desktop-portal-hyprland xdg-desktop-portal-gtk \
+	       gnome-keyring libsecret \
+	       alsa-utils pipewire wireplumber pipewire-jack pipewire-pulse \
+	       rofi-wayland \
+	       stow \
+	       kitty
+
+# HyPrPanel Installation
+sudo pacman -S brightnessctl wf-recorder hyprpicker power-profiles-daemon pacman-contrib
+yay -S ags-hyprpanel-git
+
+# Services
+sudo systemctl enable sddm
+sudo systemctl enable bluetooth
