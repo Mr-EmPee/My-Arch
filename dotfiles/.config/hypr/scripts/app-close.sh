07b08352e67cb5799d13f71e97f95c5b3863549b
Manuel Palumbo <manuel.palumbo@email.com>
2026-01-11 13:44:14 +0100

    Discord fixed window

--- dotfiles/.config/hypr/scripts/app-close.sh
+++ dotfiles/.config/hypr/scripts/app-close.sh
@@ -2,17 +2,22 @@
 
 active_class=$(hyprctl activewindow -j | jq -r '.class')
 
-if [[ "$active_class" != "Alacritty" ]]; then
-    hyprctl dispatch killactive
+if [[ "$active_class" == "Alacritty" ]]; then
+    SESSION="main"
+
+    windows=$(tmux list-windows -t "$SESSION" 2>/dev/null | wc -l)
+
+    if [[ "$windows" -le 1 ]]; then
+        exit 0
+    fi
+
+    tmux kill-window -t "$SESSION:$(tmux display-message -p '#I')"
     exit 0
 fi
 
-SESSION="main"
-
-windows=$(tmux list-windows -t "$SESSION" 2>/dev/null | wc -l)
-
-if [[ "$windows" -le 1 ]]; then
+if [[ "$active_class" == "discord" ]]; then
     exit 0
 fi
 
-tmux kill-window -t "$SESSION:$(tmux display-message -p '#I')"
+hyprctl dispatch killactive
+
