ca6a883aca12161a9d78a48b56fde5910562c9af
Manuel Palumbo <manuel.palumbo@email.com>
2026-04-20 18:46:10 +0200

    Hibernate on low battery

---
+++ dotfiles/.config/hypr/scripts/battery-check.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+BAT_PATH="/sys/class/power_supply/BAT0"
+
+if [ ! -d "$BAT_PATH" ]; then
+  BAT_PATH=$(find /sys/class/power_supply/ -maxdepth 1 -name "BAT*" | head -n 1)
+fi
+
+[ -z "$BAT_PATH" ] && exit 1
+
+while true; do
+  capacity=$(cat "$BAT_PATH/capacity")
+  status=$(cat "$BAT_PATH/status")
+
+  if [[ "$status" != "Charging" && "$capacity" -lt 6 ]]; then
+    systemctl hibernate
+  fi
+
+  sleep 60
+done
